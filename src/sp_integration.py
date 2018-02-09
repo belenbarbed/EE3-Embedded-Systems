785e482637716345ae1bb37d463c7628981118e1
belenbarbed <belenbarbed@gmail.com>
2018-02-09 19:42:27 +0000

    fixed python3 compatibility in spotipy script

--- src/sp_integration.py
+++ src/sp_integration.py
@@ -9,7 +9,7 @@ scope = 'user-library-read'
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
-    print "Usage: %s username" % (sys.argv[0],)
+    print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 token = util.prompt_for_user_token(username,scope,client_id='107ff4c93c9b4e01a848fbdba04aac5a',client_secret='354cc166e50b4317bb6e3e1a3dee7b32',redirect_uri='https://open.spotify.com/album/5OVcAB06ttNop0LKRUUKKD')
 
@@ -20,12 +20,12 @@ if token:
     results = sp.artist_top_tracks('https://open.spotify.com/artist/6PXS4YHDkKvl1wkIl4V8DL', country='US')
 
     for track in results['tracks'][:10]:
-        print 'track    : ' + track['name']
-        print 'audio    : ' + track['preview_url']
-        print 'cover art: ' + track['album']['images'][0]['url']
+        print('track    : ' + track['name'])
+        print('audio    : ' + track['preview_url'])
+        print('cover art: ' + track['album']['images'][0]['url'])
         p = vlc.MediaPlayer(track['preview_url'])
         p.play()
         time.sleep(32)
         print
 else:
-    print "Can't get token for", username
+    print ("Can't get token for", username)

a98eb5bfb4080005f7f8408af78cbca54a8a8b4b
belenbarbed <belenbarbed@gmail.com>
2018-02-11 23:03:00 +0000

    esp module connects to phone hotspot

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import machine
 import time
-#import ujson
-#import network
-#from umqtt.simple import MQTTClient
+import ujson
+import network
+from umqtt.simple import MQTTClient
 
 # read x axis
 def getX():
@@ -10,21 +10,6 @@ def getX():
 	x_lower = int.from_bytes(i2cport.readfrom_mem(24, 0x28, 1), 'little')
 	x = (x_higher << 8) + x_lower
 	return x
-
-# read y axis
-def getY():
-	y_higher = int.from_bytes(i2cport.readfrom_mem(24, 0x2B, 1), 'little')
-	y_lower = int.from_bytes(i2cport.readfrom_mem(24, 0x2A, 1), 'little')
-	y = (y_higher << 8) + y_lower
-	return y
-
-# read z axis
-def getZ():
-	z_higher = int.from_bytes(i2cport.readfrom_mem(24, 0x2D, 1), 'little')
-	z_lower = int.from_bytes(i2cport.readfrom_mem(24, 0x2C, 1), 'little')
-	z = (z_higher << 8) + z_lower
-	return z
-
 
 # ###################### CODE BEGINS HERE ##############################################
 
@@ -34,26 +19,26 @@ i2cport = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
 # enable accelerometer read at 400Hz (at reg 0x20)
 i2cport.writeto(24, bytearray([0x20, 0x77]))	
 
-'''
 # connect to WiFi
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-sta_if.connect('EEERover', 'exhibition')
+#sta_if.connect('EEERover', 'exhibition')
+sta_if.connect('Belen', 'exhibition')
 time.sleep(5)
 print(sta_if.isconnected())
 
 # connect to MQTT broker
-client = MQTTClient(machine.unique_id(), "192.168.0.10")
+#client = MQTTClient(machine.unique_id(), "192.168.0.10")
+client = MQTTClient(machine.unique_id(), "iot.eclipse.org")
 client.connect()
-'''
 
 time.sleep(2)
 
 run_freq = 400
 bpm_freq  = 15
-run_for_s = bpm_freq * 2
+run_for_s = bpm_freq * 200
 
 # isBelow: False = rising edge has been last detected
 #          True  = falling edge has been last detected
@@ -85,3 +70,6 @@ for i in range(0, run_for_s/bpm_freq):
 
 	bpm = int(peak_ps * (60/bpm_freq))
 	print("bpm: "+str(bpm))
+	dict = {'bpm': bpm}
+	data = ujson.dumps(dict)
+	client.publish('esys/The100/', bytes(data, 'utf-8'))

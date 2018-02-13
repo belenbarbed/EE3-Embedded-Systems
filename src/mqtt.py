7c148e162e40d51ff1d764d66fb729d2bea8da0e
Mwanakombo Hussein <mwanakombohussein@dyn1190-236.wlan.ic.ac.uk>
2018-02-13 11:02:47 +0000

    minor update on not playingon 1st iteration

--- src/mqtt.py
+++ src/mqtt.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 import ast
 import sp_method_connected
 
+isFirst = True
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -14,10 +16,14 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
     #print(str(msg.payload))
-    d = ast.literal_eval(msg.payload)
-    bpm = float(d['bpm'])
-    print(bpm)
-    sp_method_connected.play_music(bpm)
+    global isFirst
+
+    if(isFirst == False):
+        d = ast.literal_eval(msg.payload)
+        bpm = float(d['bpm'])
+        print(bpm)
+        sp_method_connected.play_music(bpm)
+    isFirst = False
 
 client = mqtt.Client("TrackIt")
 client.on_connect = on_connect

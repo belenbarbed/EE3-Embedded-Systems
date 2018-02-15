0f4665f311c13769642ac77d6745d228322b674f
belenbarbed <belenbarbed@gmail.com>
2018-02-15 13:32:33 +0000

    commented all my code

--- CW1-IoT/src/main.py
+++ CW1-IoT/src/main.py
@@ -4,7 +4,7 @@ import ujson
 import network
 from umqtt.simple import MQTTClient
 
-# read x axis
+# function to read x axis
 def getX():
 	x_higher = int.from_bytes(i2cport.readfrom_mem(24, 0x29, 1), 'little')
 	x_lower = int.from_bytes(i2cport.readfrom_mem(24, 0x28, 1), 'little')
@@ -16,29 +16,31 @@ def getX():
 # set up i2c comm
 i2cport = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
 
-# enable accelerometer read at 400Hz (at reg 0x20)
+# enable accelerometer read at 400Hz, only x axis activated
 i2cport.writeto(24, bytearray([0x20, 0x77]))	
 
-# connect to WiFi
+# connect to phone hotspot
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-#sta_if.connect('EEERover', 'exhibition')
 sta_if.connect('Belen', 'exhibition')
+
+# give time for connection
 time.sleep(5)
 print(sta_if.isconnected())
 
-# connect to MQTT broker
-#client = MQTTClient(machine.unique_id(), "192.168.0.10")
+# connect to eclipse iot MQTT broker
 client = MQTTClient(machine.unique_id(), "iot.eclipse.org")
 client.connect()
 
+# give time for user to start running before we count the pace
 time.sleep(2)
 
+# run_freq: frequency of axis sampling
 run_freq = 400
+# bpm_freq: how often we calculate bpm
 bpm_freq  = 15
-run_for_s = bpm_freq * 200
 
 # isBelow: False = rising edge has been last detected
 #          True  = falling edge has been last detected
@@ -52,24 +54,38 @@ while True:
 	for j in range (0, run_freq * bpm_freq):
 	
 		x = getX()
-		#print(x)
 		
+		# state 1: 
 		if((x < 20000) and (j > until) and (isBelow == False)):
+			# update peak counter
 			peak_ps += 1
+			# don't detect a rising edge until 75 cycles have passed
+			# as seen when running at 180bpm (avoids noise)
 			until = j + 75
+			# isBelow: True = falling edge has been last detected
+			# rising edge should be detected next
 			isBelow = True
 		
 		if((x > 50000) and (j > until) and (isBelow == True)):
+			# update peak counter
 			peak_ps += 1
+			# don't detect a falling edge until 125 cycles have passed
+			# as seen when running at 180bpm (avoids noise)
 			until = j + 125
+			# isBelow: False = rising edge has been last detected
+			# falling edge should be detected next
 			isBelow = False
 	
-		# total period is 2500us
+		# total period is 2500us - 400Hz
 		# computation per period takes approx 2100us
+		# sleep for 400us to maintain periodicity
 		time.sleep_us(400)
 
+	# calculate peaks per minute from peaks in 15s period
 	bpm = int(peak_ps * (60/bpm_freq))
 	print("bpm: "+str(bpm))
+	
+	# send bpm values to broker
 	dict = {'bpm': bpm}
 	data = ujson.dumps(dict)
 	client.publish('esys/The100/', bytes(data, 'utf-8'))
--- CW1-IoT/src/mqtt.py
+++ CW1-IoT/src/mqtt.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import ast
 import sp_method_connected
 
+# used to ignore first reading as user may not have been running for all of it
 isFirst = True
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -18,10 +19,13 @@ def on_message(client, userdata, msg):
     #print(str(msg.payload))
     global isFirst
 
+	# only count 2nd reading onwards
     if(isFirst == False):
+		# obtain bpm from message
         d = ast.literal_eval(msg.payload)
         bpm = float(d['bpm'])
         print(bpm)
+		# call spotipy code, will play relevant music at bpm +- 5
         sp_method_connected.play_music(bpm)
     isFirst = False
 
@@ -29,7 +33,6 @@ client = mqtt.Client("TrackIt")
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("192.168.0.10", 1883, 60)
 client.connect("iot.eclipse.org", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
@@ -37,4 +40,3 @@ client.connect("iot.eclipse.org", 1883, 60)
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
-print("Hello world")

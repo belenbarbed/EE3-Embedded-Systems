0d5ce44a23a2a7f7b1c7a79218496d31b211e189
belenbarbed <belenbarbed@gmail.com>
2018-03-23 18:46:24 +0000

    commented code thoroughly

--- CW2-Motor/main.cpp
+++ CW2-Motor/main.cpp
@@ -3,19 +3,19 @@
 #include "rtos.h"
 
 
-//Initialise the serial port
+// Initialise the serial port
 RawSerial pc(SERIAL_TX, SERIAL_RX);
 
 //---------------------------------------------------------------------- THREADS
 
-//Threads
+// Thread instantiation
 Thread MotorCtrlT(osPriorityNormal, 1024);
 Thread CommOutT(osPriorityNormal, 1024);
 Thread Decode(osPriorityNormal, 1024);
 
 //-------------------------------------------------------- FUNCTION DECLARATIONS
 
-// Motor control
+// Motor control functions
 void motorOut(int8_t driveState, uint32_t torque);
 inline int8_t readRotorState();
 int8_t motorHome();
@@ -23,15 +23,15 @@ void motorISR();
 void motorCtrlFn();
 void motorCtrlTick();
 
-// Messaging
+// Messaging functions
 void putMessage(uint8_t code, uint32_t data);
 void commOutFn();
 
-// Commands
+// Command passing functions
 void serialISR();
 void decodeFn();
 
-// Bitcoin mining
+// Bitcoin mining function
 void BitcoinFn();
 
 //----------------------------------------------- GLOBAL VARIABLE INITIALISATION
@@ -66,7 +66,7 @@ State   L1  L2  L3
 7       -   -   -
 */
 
-//a unique code for each message type
+// Unique codes for each message type
 enum messageType
 {
     start = 0,
@@ -87,8 +87,8 @@ const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};
 //Alternative if phase order of input or drive is reversed:
 //const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; 
 
-// Phase lead to make motor spin
-int8_t lead = 2;  //2 for forwards, -2 for backwards
+// Phase lead to make motor spin (2 for forwards, -2 for backwards)
+int8_t lead = 2;
 
 // Rotor offset at motor state 0
 int8_t orState = 0;
@@ -98,6 +98,9 @@ typedef struct{
     uint8_t code;
     uint32_t data; 
 } message_t ;
+
+//Initialise the mail outmessage queue
+Mail<message_t,8> outMessages;
 
 // Status LED
 DigitalOut led1(LED1);
@@ -119,30 +122,29 @@ DigitalOut L3H(L3Hpin);
 char char_array[50] = {0};
 Queue<void, 8> inCharQ;
 
-//Initialise the mail outmessage
-Mail<message_t,8> outMessages;
+// motor control golbal variables
+volatile int32_t motorPower = 500; // torque value
+volatile int32_t motorPosition;    // number of 1/6th rotations done since start
 
 // bitcoin key setting w/ command
-volatile uint64_t newKey = 0;
+volatile uint64_t newKey = 0;      // key from command K
 Mutex newKey_mutex;
 
-// motor control
-volatile int32_t motorPower = 500; // used to be uint like in spec
-volatile int32_t motorPosition;
-
 // set rotation speed w/ command
-volatile double maxSpeed = 0;
+volatile double maxSpeed = 0;      // maximum speed value from command V
 Mutex maxSpeed_mutex;
 
 // set position control w/ command
-volatile double noRotations = 0;
+volatile double noRotations = 0;   // number of rotations to perform at maxSpeed
+                                   // from command R
 Mutex noRotations_mutex;
-volatile int32_t motorPos_start;
+volatile int32_t motorPos_start;   // number of rotations done by motor from 
+                                   // start when new rotation command R comes in
 
 //------------------------------------------------------------------------- MAIN
 
 int main() {
-    pc.printf("hi, Ed\r\n");
+    pc.printf("\r\n");
     // indicate start of main code
     putMessage(start, 0);
     
@@ -174,12 +176,13 @@ int main() {
     I3.rise(&motorISR);
     I3.fall(&motorISR);
     
+    // mine forever in the background
     BitcoinFn();
 }
 
 //---------------------------------------------------------------- MOTOR CONTROL
 
-// Set a given drive state
+// Set a given drive state in PWM
 void motorOut(int8_t driveState, uint32_t torque){
     
     // Lookup the output byte from the drive state.
@@ -210,7 +213,6 @@ inline int8_t readRotorState(){
 // Basic synchronisation routine    
 int8_t motorHome() {
     //Put the motor in drive state 0 and wait for it to stabilise
-    // TODO: ask if max means 2000us or 50% duty cycle (1000us)
     motorOut(0, 2000);
     wait(1.0);
     
@@ -229,18 +231,20 @@ void motorISR () {
     // calculate PWM value from input motorPower
     if (motorPower_old != motorPower) {
         torque = motorPower;
+        // set motor direction depending on torque value
         if(torque < 0) {
             torque = -torque;
             lead = -2;
         } else {
             lead = 2;
         }
+        // set maximum duty cycle to 50%
         if(torque > 1000) torque = 1000;
-        //pc.printf("new torque: %d\r\n", torque);
     }
     motorOut((rotorState-orState+lead+6)%6, torque);
     motorPower_old = motorPower;
     
+    // update current position of motor (in 1/6th of rotations done since start)
     if (rotorState - oldRotorState == 5) {
         motorPosition--;
     } else if (rotorState - oldRotorState == -5) {
@@ -251,7 +255,7 @@ void motorISR () {
     oldRotorState = rotorState;
 }
 
-// motor controlled thread fn
+// motor controller thread fn
 void motorCtrlFn() {
     
     // execute every 100ms (0.1s)
@@ -260,12 +264,14 @@ void motorCtrlFn() {
     
     int32_t i = 0;
     
+    // velocity control variables
     int32_t velocity = 0;
     int32_t k_p_v = 100;
     int32_t diff;
     int32_t motorPos_old = motorPosition;
     int32_t motorPower_v;
     
+    // position control variables
     int32_t k_p_p = 30;
     int32_t k_d_p = 20;
     float Er;
@@ -285,6 +291,7 @@ void motorCtrlFn() {
         // Position Control
         noRotations_mutex.lock();
         Er = noRotations - (motorPosition - motorPos_start);
+        // for command R0, keep spinning forever at maxSpeed
         if(noRotations == 0) Er = 1000;
         noRotations_mutex.unlock();
         Er_d_old = Er_d;
@@ -292,8 +299,10 @@ void motorCtrlFn() {
         Er_d = Er_d * 0.9 + Er_d_old * 0.1;
         (Er < 0) ? sgn_Er = -1 : sgn_Er = 1;
         Er_old = Er;
+        // Position control PD controller
         motorPower_p = k_p_p * Er + k_d_p * Er_d;
         if(i%10 == 0){
+            // print number of rotations done every 1s
             putMessage(no_rots, (motorPosition - motorPos_start)/6);
         } 
         //
@@ -302,6 +311,7 @@ void motorCtrlFn() {
         velocity = ((motorPosition - motorPos_old)*10)/6;
         motorPos_old = motorPosition;
         if(i%10 == 0){
+            // print current velocity every 1s
             putMessage(motor_speed, velocity);
         }        
         maxSpeed_mutex.lock();
@@ -311,13 +321,18 @@ void motorCtrlFn() {
         } else {
             sgn_vel = 1;
         }
+        // diff is used to compensate for the motor
+        // going slightly slower than the target speed
+        // (values of y=mx+c explained in report)
         diff = (1.0502)*maxSpeed + 0.62;
+        // Velocity control P controler
         motorPower_v = k_p_v * (diff - velocity) * sgn_Er;
+        // for command V0, spin at fastes velocity (50% duty cycle)
         if(maxSpeed == 0) motorPower_v = 1000;
         maxSpeed_mutex.unlock();
         //
         
-        // Choose between Velocity and Position Control
+        // Choose between Velocity and Position Control (use most conservative)
         if(sgn_vel) {
             (motorPower_v < motorPower_p) ? motorPower = motorPower_v : motorPower = motorPower_p;
         } else {
@@ -334,7 +349,7 @@ void motorCtrlTick(){
 
 //-------------------------------------------------------------------- MESSAGING
 
-//putMessage - separate function that adds messages to the queue
+// Add messages to the queue
 void putMessage(uint8_t code, uint32_t data){
      //*pMessage is a pointer which points to the memory location
      //where the message will be stored
@@ -345,14 +360,13 @@ void putMessage(uint8_t code, uint32_t data){
      outMessages.put(pMessage);
 }
 
-//commOutFn - take messages from the queue and print them on the serial port
+// Take messages from the queue and print them on the serial port
 void commOutFn(){
     
     while(1) {
         osEvent newEvent = outMessages.get();
         message_t *pMessage = (message_t*)newEvent.value.p;
-        //pc.printf("%d: with data 0x%016x\n\r", pMessage->code,pMessage->data);
-        
+        // print messages differenty according to type
         switch(pMessage->code) {
             case start:
                 pc.printf("PROGRAM START --------------------\r\n");
@@ -378,24 +392,23 @@ void commOutFn(){
             default:
                 break;
         }
-        
+        // delete message from queue once printed
         outMessages.free(pMessage);
     }    
 }
 
 //--------------------------------------------------------------------- COMMANDS
 
-//SerialISR - retrieves a byte from the serial port(pc)
-//and places it on the queue 
+// Retrieves a byte from the serial port(pc) and places it on the decode queue 
 void serialISR(){
     uint8_t newChar = pc.getc();
     inCharQ.put((void*)newChar);
 }
 
-//thread to decode commands   
+// Thread to decode commands   
 void decodeFn(){  
     int array_pos = 0;
-    //attaching serialISR to the serial port(pc)
+    //attach serialISR to the serial port(pc)
     pc.attach(&serialISR);
     while(1) {
         osEvent newEvent = inCharQ.get();
@@ -409,13 +422,14 @@ void decodeFn(){
             
             if(newChar == '\r'){
                 // end of command reached, time to decode
+                // add terminate string value to end of command
                 char_array[array_pos] = '\0';
                 array_pos = 0;
                 
                 // check first character
                 switch(char_array[0]) {
                     case 'R': 
-                        // do rotation
+                        // command for number of rotations
                         noRotations_mutex.lock();
                         sscanf(char_array, "R%lf", &noRotations);
                         pc.printf("No of rotations: %lf\r\n", noRotations);
@@ -424,20 +438,20 @@ void decodeFn(){
                         motorPos_start = motorPosition;
                         break;
                     case 'V': 
-                        // do max speed
+                        // command to set maximum speed
                         maxSpeed_mutex.lock();
                         sscanf(char_array, "V%lf", &maxSpeed);
                         pc.printf("New maxSpeed: %lf\r\n", maxSpeed);
                         maxSpeed_mutex.unlock();
                         break;
                     case 'K':
-                        // set bitcoin key
+                        // command to set bitcoin key
                         newKey_mutex.lock();
                         sscanf(char_array, "K%x", &newKey);
                         newKey_mutex.unlock();
                         break;
                     default:
-                        // do nothing?
+                        // do nothing
                         break;
                 }
             }
@@ -449,7 +463,6 @@ void decodeFn(){
 
 void BitcoinFn() {
     
-    // Bitcoin stuff
     SHA256 sha;
     uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64,
                           0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73,
@@ -463,25 +476,21 @@ void BitcoinFn() {
     uint64_t* nonce = (uint64_t*)((int)sequence + 56);
     uint8_t hash[32];
     
-    uint64_t i = 0;
+    // infinite loop i the background
     while (1) {
-        
         newKey_mutex.lock();
         if (newKey != *key) {
             *key = newKey;
-            //pc.printf("newKey: %lu\n\r", *key);
-            i = 0;
+            *nonce = 0;
         }
         newKey_mutex.unlock();
-        
-        *nonce = i;
+
         sha.computeHash(hash, sequence, 64);
         if ((hash[0] || hash[1]) == 0) {
             // Send message reporting good nonce
             uint32_t nonce_lower  = (uint32_t) *nonce;
             putMessage(bitcoinNonce_lower, nonce_lower);
-            //pc.printf("nonce found: %ul\n\r", *nonce);
         }
-        i++;
-    }  
+        (*nonce)++;
+    }
 }
